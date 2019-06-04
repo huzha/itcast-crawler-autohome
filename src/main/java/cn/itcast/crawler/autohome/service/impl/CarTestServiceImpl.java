7418ca23b4dd4478a313fd3926e9b8092105a756
瞿安林 <quentin@jollycorp.com>
2019-06-04 21:39:42 +0800

    autowired改为resource

--- src/main/java/cn/itcast/crawler/autohome/service/impl/CarTestServiceImpl.java
+++ src/main/java/cn/itcast/crawler/autohome/service/impl/CarTestServiceImpl.java
@@ -3,16 +3,16 @@ package cn.itcast.crawler.autohome.service.impl;
 import cn.itcast.crawler.autohome.mapper.CarTestMapper;
 import cn.itcast.crawler.autohome.pojo.CarTest;
 import cn.itcast.crawler.autohome.service.CarTestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class CarTestServiceImpl implements CarTestService {
-    @Autowired
+    @Resource
     private CarTestMapper carTestMapper;
 
     @Override
